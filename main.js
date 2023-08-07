d3523db2910169ae2bbad75d55b1673a354f8a97
nghi01 <nnguyen@oberlin.edu>
2023-08-07 16:37:39 -0400

    basic screen

---
+++ main.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("screen");
+var ctx = canvas.getContext("2d");
+
+var width = canvas.width;
+var height = canvas.height;
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, width, height);
