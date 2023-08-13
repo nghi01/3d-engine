99ff7309d337f1bd3ea4b13f2c45a3a82a07a53c
nghi01 <nnguyen@oberlin.edu>
2023-08-13 15:31:53 -0400

    Fix draw error

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-windows.ECS = {
+window.ECS = {
     entities: [],
     components: {},
     systems: {}
 };
 
-ECS.canvas = document.getElementById("screen");
-ECS.context = ECS.canvas.getContext("2d");
-ECS.width = canvas.width;
-ECS.height = canvas.height;
+ECS.$canvas = document.getElementById("screen");
+ECS.context = ECS.$canvas.getContext("2d");
+ECS.width = ECS.$canvas.width;
+ECS.height = ECS.$canvas.height;
 
 ECS.context.fillStyle = "#337cc4";
-ECS.context.fillRect(0, 0, width, height);
+ECS.context.fillRect(0, 0, ECS.width, ECS.height);
